936644cc9c09d5d1164db2e508aa289ec0a78bb0
Grigore Budac <contact@grigorebudac.com>
2022-09-18 19:39:12 +0200

    create a context wrapper for builder

--- apps/app/src/modules/builder/pages/Builder/Builder.tsx
+++ apps/app/src/modules/builder/pages/Builder/Builder.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { BlockManager, BasicType } from 'easy-email-core';
 import { EmailEditor, EmailEditorProvider } from 'easy-email-editor';
 import { StandardLayout } from 'easy-email-extensions';
 import { useWindowSize } from 'react-use';
 
 import 'easy-email-editor/lib/style.css';
 import 'easy-email-extensions/lib/style.css';
-
 import '@arco-themes/react-easy-email-theme/css/arco.css';
 
-const initialValues = {
-  subject: 'Welcome to Easy-email',
-  subTitle: 'Nice to meet you!',
-  content: BlockManager.getBlockByType(BasicType.PAGE)!.create({}),
-};
+import {
+  BuilderContextProvider,
+  BuilderContext,
+} from '../../contexts/BuilderContext';
 
 const Builder = () => {
   const { width } = useWindowSize();
@@ -21,23 +18,29 @@ const Builder = () => {
   const smallScene = width < 1400;
 
   return (
-    <EmailEditorProvider
-      data={initialValues}
-      height={'calc(100vh - 72px)'}
-      autoComplete
-      dashed={false}
-    >
-      {({ values }) => {
-        return (
-          <>
-            {/* @ts-ignore */}
-            <StandardLayout compact={!smallScene} showSourceCode={true}>
-              <EmailEditor />
-            </StandardLayout>
-          </>
-        );
-      }}
-    </EmailEditorProvider>
+    <BuilderContextProvider>
+      <BuilderContext.Consumer>
+        {({ initialValues }) => (
+          <EmailEditorProvider
+            data={initialValues}
+            height={'calc(100vh - 72px)'}
+            autoComplete
+            dashed={false}
+          >
+            {({ values }) => {
+              return (
+                <>
+                  {/* @ts-ignore */}
+                  <StandardLayout compact={!smallScene} showSourceCode={true}>
+                    <EmailEditor />
+                  </StandardLayout>
+                </>
+              );
+            }}
+          </EmailEditorProvider>
+        )}
+      </BuilderContext.Consumer>
+    </BuilderContextProvider>
   );
 };
 
---
+++ apps/app/src/modules/builder/types/builder.types.ts
@@ -0,0 +1,9 @@
+import { IBlockData } from 'easy-email-core';
+
+export declare namespace Builder {
+  export type InitialValues = {
+    subject: string;
+    subTitle: string;
+    content: IBlockData;
+  };
+}
