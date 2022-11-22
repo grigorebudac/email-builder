bc34c893e442e07e61e0185de09811f710bd4d92
Grigore Budac <contact@grigorebudac.com>
2022-11-22 14:21:00 +0100

    delete and edit tests

---
+++ apps/app-e2e/src/e2e/templates/delete-template.cy.ts
@@ -0,0 +1,19 @@
+describe('Delete Template', () => {
+  before('does load the page', () => {
+    cy.login();
+  });
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('does create template when valid credentials are entered', () => {
+    cy.intercept('DELETE', `/api/templates/*`).as('deleteTemplateRequest');
+
+    cy.get('[data-cy^="deleteTemplate-"]').first().click();
+
+    cy.wait('@deleteTemplateRequest')
+      .its('response.statusCode')
+      .should('eq', 200);
+  });
+});
