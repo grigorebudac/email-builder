3a553520a91dd78e15d53220be12b50ded13e0d0
bandreit <andy27hush@gmail.com>
2022-12-09 11:14:03 +0100

    change back to AWS SDK and remove nodemailer

--- apps/api/src/emails/emails.service.ts
+++ apps/api/src/emails/emails.service.ts
@@ -1,41 +1,58 @@
 import { HttpException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { SendEmailDto } from './dto/send-email.dto';
-import * as aws from '@aws-sdk/client-ses';
-import { createTransport } from 'nodemailer';
-import { SentMessageInfo } from 'nodemailer/lib/ses-transport';
+import { SES } from 'aws-sdk';
 
 const FROM_ADDRESS = 'abostan.ir@gmail.com';
 
 @Injectable()
 export class EmailsService {
+  ses: SES;
+
+  constructor(private config: ConfigService) {
+    this.ses = new SES({
+      apiVersion: '2010-12-01',
+      region: 'us-east-1',
+      accessKeyId: this.config.get('AWS_S3_ACCESS_KEY'),
+      secretAccessKey: this.config.get('AWS_S3_KEY_SECRET'),
+    });
+  }
+
   async send(sendEmailDto: SendEmailDto) {
     try {
-      const ses = new aws.SES({
-        apiVersion: '2010-12-01',
-        region: 'us-east-1',
-      });
+      const params: SES.SendEmailRequest = {
+        Destination: {
+          ToAddresses: [sendEmailDto.toAddress],
+        },
+        Message: {
+          Body: {
+            Text: {
+              Charset: 'UTF-8',
+              Data: sendEmailDto.body.text,
+            },
+            Html: {
+              Charset: 'UTF-8',
+              Data: sendEmailDto.body.html,
+            },
+          },
+          Subject: {
+            Charset: 'UTF-8',
+            Data: sendEmailDto.subject,
+          },
+        },
+        Source: FROM_ADDRESS,
+      };
 
-      const transporter = createTransport({
-        SES: { ses, aws },
-      });
+      const result = await this.ses.sendEmail(params).promise();
 
-      const response = await transporter.sendMail({
-        from: FROM_ADDRESS,
-        to: sendEmailDto.toAddress,
-        inReplyTo: FROM_ADDRESS,
-        subject: sendEmailDto.subject,
-        html: sendEmailDto.body.html,
-        text: sendEmailDto.body.text,
-      });
-
-      return { messageId: response.messageId };
+      return { messageId: result.MessageId };
     } catch (error) {
       throw new HttpException(
         {
-          status: error.$metadata.httpStatusCode,
+          status: error.statusCode,
           error: error.message,
         },
-        error.$metadata.httpStatusCode
+        error.statusCode
       );
     }
   }
